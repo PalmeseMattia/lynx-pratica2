d8378ae0fd1a2e66e9f58b2110b576a94a6d3366
andrei.maftean <andreimaftean@gmail.com>
2022-11-08 15:18:02 +0100

    primo commit

---
+++ src/main/java/com/example/lynx_pratica2/LynxPratica2Application.java
@@ -0,0 +1,13 @@
+package com.example.lynx_pratica2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LynxPratica2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LynxPratica2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lynx_pratica2/LynxPratica2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lynx_pratica2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LynxPratica2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
